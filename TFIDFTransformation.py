7192494305dd0ca3365113c6e849336c989b904e
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-03-07 20:02:54 +0530

    Remove main functions from vectorization and transformation

--- TFIDFTransformation.py
+++ TFIDFTransformation.py
@@ -1,7 +1,4 @@
 from sklearn.feature_extraction.text import TfidfTransformer
-from DataInput import readData
-from Vectorization import vectorizeTextData
-
 
 def tfidfTransformData(vectorized_data):
     '''Calculate Term Frequency And Inverse Document Frequency'''
@@ -10,14 +7,3 @@ def tfidfTransformData(vectorized_data):
     tfidf_transformed_data = tfidf_transformer_fit.transform(vectorized_data)
 
     return tfidf_transformed_data
-
-
-def main():
-    df = readData(path='SMSSpamCollection')
-    vectorized_data = vectorizeTextData(df['message'])
-    tfidf_transformed_data = tfidfTransformData(vectorized_data)
-    print(tfidf_transformed_data.shape)
-
-
-if __name__ == '__main__':
-    main()
--- Vectorization.py
+++ Vectorization.py
@@ -1,6 +1,5 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from DataPreprocessing import preprocessText
-from DataInput import readData
 
 import pandas as pd
 
@@ -12,13 +11,3 @@ def vectorizeTextData(data):
     vectorizer_transform = vectorizer_fit.transform(data)
 
     return vectorizer_transform
-
-
-def main():
-    df = readData(path='SMSSpamCollection')
-    vectorizer_transform = vectorizeTextData(df['message'])
-    print(vectorizer_transform)
-
-
-if __name__ == '__main__':
-    main()
