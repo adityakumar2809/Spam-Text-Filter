9c4fbf8d763eea387ce766e72e89a324af56e44d
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-03-07 18:31:48 +0530

    Add Data Preprocessing step to remove stopwords

--- SpamFilter.py
+++ SpamFilter.py
@@ -15,9 +15,23 @@ def removePunctuations(text):
     return unpunctuated_text
 
 
+def removeStopwords(text):
+    '''Remove all stopwords in a piece of unpunctuated text'''
+    text_list = list(text.split(' '))
+    clean_text_list = [
+        x for x in text_list if x not in stopwords.words('english')
+    ]
+    
+    return clean_text_list
+
+
+
 def main():
     text = 'Hi!! I am, a good boy.'
     unpunctuated_text = removePunctuations(text)
+    print(f'Unpuntuated Text: {unpunctuated_text}')
+    clean_text_list = removeStopwords(unpunctuated_text)
+    print(f'Clean Text List: {clean_text_list}')
 
 
 if __name__ == '__main__':
